943793e1014d6dcf8495a6c63a70cd0be65c5f3f
JH <jyjy9734@gmail.com>
2020-01-23 20:19:13 +0900

    200123 widget first screen updated

--- Draw/app/src/main/java/com/example/draw/widget.java
+++ Draw/app/src/main/java/com/example/draw/widget.java
@@ -1,8 +1,11 @@
 package com.example.draw;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.widget.RemoteViews;
 
 /**
@@ -10,13 +13,14 @@ import android.widget.RemoteViews;
  */
 public class widget extends AppWidgetProvider {
 
+
+
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -26,7 +30,14 @@ public class widget extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+            Intent intent = new Intent(context,MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,intent,0);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(),R.layout.widget);
+            views.setOnClickPendingIntent(R.id.imgBtn_tag01, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId,views);
+
         }
     }
 
