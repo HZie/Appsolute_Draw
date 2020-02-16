5187b17b1ccf644504f114909ee477dced0a53ec
JH <jyjy9734@gmail.com>
2020-02-16 18:26:41 +0900

    200216: unfinished widget2

--- Draw/app/src/main/java/com/example/draw/widget.java
+++ Draw/app/src/main/java/com/example/draw/widget.java
@@ -5,7 +5,6 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.widget.RemoteViews;
 
 /**
@@ -26,15 +25,23 @@ public class widget extends AppWidgetProvider {
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
+    public PendingIntent getPendingSelfIntent(Context context, String action){
+        Intent intent = new Intent(context, widget2.class);
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context,0,intent,0);
+    }
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            Intent intent = new Intent(context,MainActivity.class);
+            Intent intent = new Intent(context,widget2.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,intent,0);
+
 
             RemoteViews views = new RemoteViews(context.getPackageName(),R.layout.widget);
             views.setOnClickPendingIntent(R.id.imgBtn_tag01, pendingIntent);
+
 
             appWidgetManager.updateAppWidget(appWidgetId,views);
 
